c548df5411e03e72094278d0f903107ec360b2e8
Mark Seymour <mark.seymour.ns@gmail.com>
2019-03-18 14:20:41 -0300

    Update index & config, add assets

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,27 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Hero from "../components/layout/content/hero"
 import Main from "../components/layout/content/main"
-import Image from "../components/image"
+import Section from "../components/layout/content/section"
+// import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`mark seymour`, `creative`, `full stack`, `front-end`, `back-end`, `devops`, `web developer`, `personal`, `portfolio`]} />
     <Hero>
-      <h1>Hi people</h1>
+      <h1><span>Mark Seymour</span> <span>the <em>creative</em> Full-Stack Web Developer</span></h1>
     </Hero>
     <Main>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        <Image />
-      </div>
-      <Link to="/page-2/">Go to page 2</Link>
+      <Section modifiers={[`welcome`]}>
+        <p>I am a <mark>creative web developer</mark> living in Halifax, Canada with over a decade of professional and personal experience working with full-stack web technologies.</p>
+        <p>Want to chat? Let&rsquo;s talk.</p>
+      </Section>
+      <Section>
+        <p>Secondary section!</p>
+      </Section>
     </Main>
   </Layout>
 )
