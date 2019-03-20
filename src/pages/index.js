d4ef4cf50c2f57dbea950c79a346c1fb47955600
Mark Seymour <mark.seymour.ns@gmail.com>
2019-03-20 16:31:04 -0300

    Add more homepage content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-// import { Link } from "gatsby"
+import { Link, withPrefix } from "gatsby"
 
 import Layout from "../components/layout"
 import Hero from "../components/layout/content/hero"
@@ -18,8 +18,20 @@ const IndexPage = () => (
     </Hero>
     <Main>
       <Section modifiers={[`welcome`]}>
-        <div class="container mx-auto my-8">
-          <h1>I am a creative web developer living in Halifax, Canada with over a decade of professional and personal experience working with full-stack web technologies.</h1>
+        <div class="container mx-auto my-8 px-4">
+          <h1 class="welcome-text">I am a creative web developer living in Halifax, Canada with over a decade of professional and personal experience working with full-stack web technologies.</h1>
+          <a target="_blank" class='resume-download' href={withPrefix(`mark_seymour_resume.pdf`)}>Download Résumé</a>
+        </div>
+      </Section>
+      <Section modifiers={[`portfolio`]}>
+        <div class="container mx-auto px-4 text-center">
+          <h1 class="mb-4">Portfolio <span class="text-sm">(Work in progress)</span></h1>
+          <p class="text-lg">In the meantime, check out these sites that I&rsquo;ve built independently in the past:</p>
+          <ul class="links text-lg">
+            <li class="links_item"><a href="http://maritimesysmen.com/">maritimesysmen.com</a> (Laravel, Sass, custom development)</li>
+            <li class="links_item"><a href="http://crossborderchallenge.com/">crossborderchallenge.com</a> (PHP, custom development)</li>
+          </ul>
+          <p class="text-lg">And check out my GitHub repositories for projects that I&rsquo;ve contributed to or worked on: <a href="https://github.com/mseymour">github.com/mseymour</a></p>
         </div>
       </Section>
       <Section modifiers={[`contact`]}>
